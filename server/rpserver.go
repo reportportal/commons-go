4d7a70b2febd231a805e035f3149527b0d0e187b
Andrei Varabyeu <andrei_varabyeu@epam.com>
2018-09-26 15:09:22 +0300

    rewrite import paths

--- server/rpserver.go
+++ server/rpserver.go
@@ -3,8 +3,8 @@ package server
 import (
 	"github.com/go-chi/chi"
 	"github.com/go-chi/chi/middleware"
-	"github.com/reportportal/commons-go/commons"
-	"github.com/reportportal/commons-go/conf"
+	"gopkg.in/reportportal/commons-go.v5/commons"
+	"gopkg.in/reportportal/commons-go.v5/conf"
 	"log"
 	"net/http"
 	"strconv"
